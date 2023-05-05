ac4d1c98955f22f748218e32ac3a5f7a68a69490
dinodtharinda <dinodtharinda2001@gmail.com>
2023-05-05 10:25:19 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log('Hello world')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.port|| 5000
+
+const app = express()
+
+app.use('/api/goals', require('./route/goalRoutes'))
+
+
+app.listen(port,()=>console.log(`Server started on port ${port}`))
